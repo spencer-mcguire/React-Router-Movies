2de6c7efeb176e6766f61da66ead0d0a132f40cf
spencer-mcguire <smcguire1826@gmail.com>
2019-11-11 17:32:54 -0600

    add dynamic linking

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -3,53 +3,53 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 
 const MovieList = props => {
-	const [movies, setMovies] = useState([]);
-	useEffect(() => {
-		const getMovies = () => {
-			axios
-				.get("http://localhost:5000/api/movies")
-				.then(response => {
-					setMovies(response.data);
-				})
-				.catch(error => {
-					console.error("Server Error", error);
-				});
-		};
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    const getMovies = () => {
+      axios
+        .get("http://localhost:5000/api/movies")
+        .then(response => {
+          setMovies(response.data);
+        })
+        .catch(error => {
+          console.error("Server Error", error);
+        });
+    };
 
-		getMovies();
-	}, []);
+    getMovies();
+  }, []);
 
-	return (
-		<div className='movie-list'>
-			{movies.map(movie => (
-				<Link to={`/movies/${movies.id}`}>
-					<MovieDetails key={movie.id} movie={movie} />
-				</Link>
-			))}
-		</div>
-	);
+  return (
+    <div className="movie-list">
+      {movies.map(movie => (
+        <Link to={`/movies/${movie.id}`}>
+          <MovieDetails key={movie.id} movie={movie} />
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 function MovieDetails({ movie }) {
-	const { title, director, metascore, stars } = movie;
-	return (
-		<div className='movie-card'>
-			<h2>{title}</h2>
-			<div className='movie-director'>
-				Director: <em>{director}</em>
-			</div>
-			<div className='movie-metascore'>
-				Metascore: <strong>{metascore}</strong>
-			</div>
-			<h3>Actors</h3>
+  const { title, director, metascore, stars } = movie;
+  return (
+    <div className="movie-card">
+      <h2>{title}</h2>
+      <div className="movie-director">
+        Director: <em>{director}</em>
+      </div>
+      <div className="movie-metascore">
+        Metascore: <strong>{metascore}</strong>
+      </div>
+      <h3>Actors</h3>
 
-			{stars.map(star => (
-				<div key={star} className='movie-star'>
-					{star}
-				</div>
-			))}
-		</div>
-	);
+      {stars.map(star => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default MovieList;
