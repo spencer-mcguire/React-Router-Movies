e3c2fee5bac45d38dde56417d3cb1e548ae1546d
spencer-mcguire <smcguire1826@gmail.com>
2019-11-12 16:34:26 -0600

    Saved Movies Link

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const SavedList = props => {
   const routeToHome = () => {
@@ -8,7 +9,9 @@ const SavedList = props => {
     <div className="saved-list">
       <h3>Saved Movies:</h3>
       {props.list.map(movie => (
-        <span className="saved-movie">{movie.title}</span>
+        <span className="saved-movie">
+          <NavLink to={`/movies/${movie.id}`}>{movie.title}</NavLink>
+        </span>
       ))}
       <button onClick={routeToHome} className="home-button">
         Home
